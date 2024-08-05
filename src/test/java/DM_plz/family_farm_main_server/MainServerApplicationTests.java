d027c4e88a292e7de1eb97ae02c039d85b6d1369
peanutyumyum <jrimit@gmail.com>
2024-08-05 21:38:03 +0900

    feat: 개발환경 세팅 완료

---
+++ src/test/java/DM_plz/family_farm_main_server/MainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package DM_plz.family_farm_main_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
