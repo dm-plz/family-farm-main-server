1aeeaefa688b005c5d440b20aab621ff24299624
yunsseong <aster20@hanyang.ac.kr>
2024-08-30 19:46:16 +0900

    chore: delete no longer needed class

--- src/main/java/DM_plz/family_farm_main_server/auth/oauth/application/OAuthService.java
+++
@@ -1,8 +0,0 @@
-package DM_plz.family_farm_main_server.auth.oauth.application;
-
-public interface OAuthService {
-
-	public String getAccessToken(String authorizationCode);
-
-	public String getUserInfoUri();
-}
