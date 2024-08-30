59fd56b8ce568873efb4d66a47a50bd3b62c15dc
yunsseong <aster20@hanyang.ac.kr>
2024-08-30 23:45:04 +0900

    feat: auth exception 추가

---
+++ src/main/java/DM_plz/family_farm_main_server/common/exception/exception/AuthException.java
@@ -0,0 +1,13 @@
+package DM_plz.family_farm_main_server.common.exception.exception;
+
+import DM_plz.family_farm_main_server.common.exception.errorCode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AuthException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+	private final Object data;
+}
