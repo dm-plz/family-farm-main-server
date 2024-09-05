58e8f4d4555e9d5a17d6c86285eea81ae5ee0f72
yunsseong <aster20@hanyang.ac.kr>
2024-09-06 01:44:45 +0900

    fix: familyException handler 복구

--- src/main/java/DM_plz/family_farm_main_server/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/DM_plz/family_farm_main_server/common/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import DM_plz.family_farm_main_server.common.exception.ErrorResponse;
 import DM_plz.family_farm_main_server.common.exception.errorCode.ErrorCode;
 import DM_plz.family_farm_main_server.common.exception.exception.CommonException;
+import DM_plz.family_farm_main_server.common.exception.exception.FamilyException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -18,10 +19,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(errorCode, errorCode.getMessage(), e.getData());
 	}
 
+	@ExceptionHandler(FamilyException.class)
+	public ResponseEntity<ErrorResponse> commonError(FamilyException e) {
+		ErrorCode errorCode = e.getErrorCode();
+		return handleExceptionInternal(errorCode, errorCode.getMessage());
+	}
+
 	/**
 	 * Exception에서 data를 사용하지 않는 경우에 사용한다.
 	 */
-	private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+	private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode, String message) {
 		return ResponseEntity.status(errorCode.getHttpStatus())
 			.body(makeErrorResponse(errorCode, message));
 	}
