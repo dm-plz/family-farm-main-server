27ef511f1169cb7b50756688c461549a29ab4e39
yunsseong <aster20@hanyang.ac.kr>
2024-09-11 14:04:40 +0900

    fix: 토큰 재발급 로직 변경으로 테스트 코드 변경

--- src/test/java/DM_plz/family_farm_main_server/auth/token/application/TokenProviderTest.java
+++ src/test/java/DM_plz/family_farm_main_server/auth/token/application/TokenProviderTest.java
@@ -11,15 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
+
 import DM_plz.family_farm_main_server.auth.api.AuthController;
 import DM_plz.family_farm_main_server.auth.application.IdTokenService;
 import DM_plz.family_farm_main_server.auth.application.JwkService;
 import DM_plz.family_farm_main_server.auth.dto.CustomAuthentication;
 import DM_plz.family_farm_main_server.auth.dto.JwtSet;
+import DM_plz.family_farm_main_server.auth.dto.RefreshTokenDTO;
 import DM_plz.family_farm_main_server.auth.token.dao.TokenRepository;
 import DM_plz.family_farm_main_server.auth.token.domain.Token;
+import DM_plz.family_farm_main_server.common.exception.exception.TokenException;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.persistence.EntityNotFoundException;
 
 @SpringBootTest
@@ -45,6 +48,8 @@ class TokenProviderTest {
 
 	private Long userId = 1L;
 	private Long familyId = 2L;
+	private int expectUserId = 1;
+	private int expectFamilyId = 2;
 	private int accessTokenExpireTime = 1000 * 3;
 
 	private int refreshTokenExpireTime = 1000 * 6;
@@ -79,7 +84,6 @@ class TokenProviderTest {
 
 		//When
 		String accessToken = tokenProvider.generateAccessToken(authentication, accessTokenExpireTime);
-		String refreshToken = tokenProvider.generateRefreshToken(authentication, refreshTokenExpireTime);
 		boolean validateResult = tokenProvider.validateToken(accessToken);
 		Claims claims = tokenProvider.parseClaim(accessToken);
 
@@ -87,8 +91,8 @@ class TokenProviderTest {
 		assertThat(validateResult).isEqualTo(true);
 		assertThat(claims.getSubject()).isEqualTo(subMockUp);
 		assertThat(claims.getExpiration()).isEqualTo(expectExpireTime);
-		assertThat(claims.get("user-id")).isEqualTo(userId);
-		assertThat(claims.get("family-id")).isEqualTo(familyId);
+		assertThat(claims.get("user-id")).isEqualTo(expectUserId);
+		assertThat(claims.get("family-id")).isEqualTo(expectFamilyId);
 	}
 
 	@Test
@@ -114,14 +118,14 @@ class TokenProviderTest {
 		assertThat(validateResult).isEqualTo(true);
 		assertThat(claims.getSubject()).isEqualTo(subMockUp);
 		assertThat(claims.getExpiration()).isEqualTo(expectRefreshTokenExpireTime);
-		assertThat(claims.get("user-id")).isEqualTo(userId);
-		assertThat(claims.get("family-id")).isEqualTo(familyId);
+		assertThat(claims.get("user-id")).isEqualTo(expectUserId);
+		assertThat(claims.get("family-id")).isEqualTo(expectFamilyId);
 
 	}
 
 	@Test
 	@DisplayName("만료된 access token 검증 시 오류 발생 확인")
-	void validateNoMatchMemberToken() throws InterruptedException {
+	void validateExpiredToken() throws InterruptedException {
 		//Given
 		String subMockUp = "SUB_MOCKUP3";
 		CustomAuthentication authentication = new CustomAuthentication(subMockUp, userId, familyId);
@@ -132,21 +136,7 @@ class TokenProviderTest {
 		Throwable thrown = catchThrowable(() -> tokenProvider.validateToken(accessToken));
 
 		//Then
-		assertThat(thrown).isInstanceOf(ExpiredJwtException.class);
-	}
-
-	@Test
-	@DisplayName("유효하지 않은 사용자의 access token 검증 시 오류 발생 확인")
-	void validateExpiredToken() throws InterruptedException {
-		//Given
-		String subMockUp = "SUB_MOCKUP4";
-		CustomAuthentication authentication = new CustomAuthentication(subMockUp, userId, familyId);
-		String accessToken = tokenProvider.generateAccessToken(authentication, accessTokenExpireTime);
-		//When
-		Throwable thrown = catchThrowable(() -> tokenProvider.validateToken(accessToken));
-
-		//Then
-		assertThat(thrown).isInstanceOf(EntityNotFoundException.class);
+		assertThat(thrown).isInstanceOf(TokenExpiredException.class);
 	}
 
 	@Test
@@ -155,13 +145,12 @@ class TokenProviderTest {
 		//Given
 		String subMockUp = "SUB_MOCKUP5";
 		CustomAuthentication authentication = new CustomAuthentication(subMockUp, userId, familyId);
-		String accessToken = tokenProvider.generateAccessToken(authentication, accessTokenExpireTime);
 		String refreshToken = tokenProvider.generateRefreshToken(authentication,
 			accessTokenExpireTime * 10);
 
 		//When
 		Thread.sleep(accessTokenExpireTime);
-		JwtSet jwtSet = tokenProvider.reissueToken(accessToken, refreshToken);
+		JwtSet jwtSet = tokenProvider.reissueToken(new RefreshTokenDTO(refreshToken));
 		boolean validateAccessTokenResult = tokenProvider.validateToken(jwtSet.getAccessToken());
 		boolean validateRefreshTokenResult = tokenProvider.validateToken(jwtSet.getRefreshToken());
 
@@ -173,12 +162,12 @@ class TokenProviderTest {
 		assertThat(validateRefreshTokenResult).isEqualTo(true);
 
 		assertThat(accessTokenClaims.getSubject()).isEqualTo(subMockUp);
-		assertThat(accessTokenClaims.get("user-id")).isEqualTo(userId);
-		assertThat(accessTokenClaims.get("family-id")).isEqualTo(familyId);
+		assertThat(accessTokenClaims.get("user-id")).isEqualTo(expectUserId);
+		assertThat(accessTokenClaims.get("family-id")).isEqualTo(expectFamilyId);
 
-		assertThat(accessTokenClaims.getSubject()).isEqualTo(subMockUp);
-		assertThat(accessTokenClaims.get("user-id")).isEqualTo(userId);
-		assertThat(accessTokenClaims.get("family-id")).isEqualTo(familyId);
+		assertThat(refreshTokenClaims.getSubject()).isEqualTo(subMockUp);
+		assertThat(refreshTokenClaims.get("user-id")).isEqualTo(expectUserId);
+		assertThat(refreshTokenClaims.get("family-id")).isEqualTo(expectFamilyId);
 
 		Token token = tokenRepository.findByRefreshToken(jwtSet.getRefreshToken())
 			.orElseThrow(EntityNotFoundException::new);
@@ -188,24 +177,17 @@ class TokenProviderTest {
 	@Test
 	@DisplayName("access token도 만료되고 refresh token도 만료된 경우")
 	void expiredRefreshToken() throws InterruptedException {
+		//Given
 		String subMockUp = "SUB_MOCKUP6";
 		CustomAuthentication authentication = new CustomAuthentication(subMockUp, userId, familyId);
-		String accessToken = tokenProvider.generateAccessToken(authentication, accessTokenExpireTime);
 		String refreshToken = tokenProvider.generateRefreshToken(authentication, refreshTokenExpireTime);
 
 		//When
-		Thread.sleep(accessTokenExpireTime);
-		JwtSet jwtSet = tokenProvider.reissueToken(accessToken, refreshToken);
-		boolean validateResult = tokenProvider.validateToken(jwtSet.getRefreshToken());
+		Thread.sleep(refreshTokenExpireTime);
+		Throwable throwable = catchThrowable(() -> tokenProvider.reissueToken(new RefreshTokenDTO(refreshToken)));
 
 		//Then
-		Claims claims = tokenProvider.parseClaim(jwtSet.getAccessToken());
-		assertThat(validateResult).isEqualTo(true);
-		assertThat(claims.getSubject()).isEqualTo(subMockUp);
-		assertThat(claims.get("user-id")).isEqualTo(userId);
-		assertThat(claims.get("family-id")).isEqualTo(familyId);
+		assertThat(throwable).isInstanceOf(TokenException.class);
 
-		Token token = tokenRepository.findByAccessToken(reissueAccessToken).orElseThrow(EntityNotFoundException::new);
-		assertThat(token.getAccessToken()).isEqualTo(reissueAccessToken);
 	}
 }
