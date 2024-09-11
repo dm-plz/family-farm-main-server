f4759aa77bf4a545753ab0de92e6104344598902
yunsseong <aster20@hanyang.ac.kr>
2024-09-11 11:34:17 +0900

    fix: @Repository 어노테이션 삭제

--- src/main/java/DM_plz/family_farm_main_server/auth/token/dao/TokenRepository.java
+++ src/main/java/DM_plz/family_farm_main_server/auth/token/dao/TokenRepository.java
@@ -3,11 +3,9 @@ package DM_plz.family_farm_main_server.auth.token.dao;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import DM_plz.family_farm_main_server.auth.token.domain.Token;
 
-@Repository
 public interface TokenRepository extends CrudRepository<Token, String> {
 
 	Optional<Token> findByRefreshToken(String refreshToken);
